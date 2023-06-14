0a08da0255aab616d78d001ce5b1b00ce4236af8
renzo <renzo.silva96@hotmail.com>
2023-06-13 21:10:05 -0300

    realice las correcciones correspondientes.

--- ecommerce/src/components/ItemDetailContainer/ItemDetailContainer.js
+++ ecommerce/src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -14,7 +14,7 @@ const ItemDetailContainer = () => {
     useEffect(() =>{
         setLoading (true)
 
-        const docRef = doc (db, 'products', itemId)
+        const docRef = doc (db, 'items', itemId)
 
         getDoc (docRef)
             .then (response => {
--- ecommerce/src/components/ItemListContainer/ItemListContainer.js
+++ ecommerce/src/components/ItemListContainer/ItemListContainer.js
@@ -15,8 +15,8 @@ const ItemListContainer = ( {greeting }) => {
         setLoading (true)
 
     const collectionRef = categoryId
-        ? query (collection (db, 'products'), where ('category', '==', categoryId ))
-        : collection (db, 'products')
+        ? query (collection (db, 'items'), where ('category', '==', categoryId ))
+        : collection (db, 'items')
 
     getDocs (collectionRef)
         .then (response => {
